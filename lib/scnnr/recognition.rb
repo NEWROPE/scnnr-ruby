140ea118a6cc7bc0fe66a58b989f5253b0dcf8e1
Patrick Ofilada <pcofilada@gmail.com>
2018-01-22 20:32:46 +0800

    Use image attributes from parsed JSON response

--- lib/scnnr/recognition.rb
+++ lib/scnnr/recognition.rb
@@ -2,10 +2,11 @@
 
 module Scnnr
   class Recognition
-    attr_reader :id, :objects, :state, :error
+    attr_reader :id, :image, :objects, :state, :error
 
     def initialize(attrs = {})
       @id = attrs['id']
+      @image = Image.new(attrs['image']) if attrs['image']
       @objects = (attrs['objects'] || []).map { |obj| Object.new(obj) }
       @state = attrs['state']&.intern
       @error = attrs['error']
@@ -24,7 +25,9 @@ module Scnnr
     end
 
     def to_h
-      { 'id' => self.id, 'objects' => self.objects.map(&:to_h), 'state' => self.state.to_s }
+      hash = { 'id' => self.id, 'objects' => self.objects.map(&:to_h), 'state' => self.state.to_s }
+      hash['image'] = self.image.to_h if self.image
+      hash
     end
   end
 end
